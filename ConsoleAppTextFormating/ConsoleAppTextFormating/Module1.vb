2935fa41ac844e23a9b7b8372239b52d4190501a
18PY0606 <18PY0606@L2222>
2018-10-12 12:00:16 +0100

    rng char enter to escape

---
+++ ConsoleAppTextFormating/ConsoleAppTextFormating/Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Randomize()
+
+        Dim rand As New Random
+        Dim a As Integer
+        Dim i As ConsoleKeyInfo = Console.ReadKey()
+
+
+        While True
+
+            a = rand.Next(65, 122)
+            Console.WriteLine(Chr(a))
+            i = Console.ReadKey()
+            Console.Clear()
+            If (i.Key) = 13 Then
+
+                Exit While
+
+            End If
+
+        End While
+
+    End Sub
+
+End Module
