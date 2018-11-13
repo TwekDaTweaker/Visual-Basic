ebffe679c3226f5cdcc9b789e02418798916258d
18py0606 <18py0606@L2226>
2018-11-13 16:01:38 +0000

    TooLong

---
+++ ConsoleAppIsTooLong/ConsoleAppIsTooLong/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'Console.WindowWidth = 20
+
+        Console.WriteLine(TooLong(Console.ReadLine()))
+
+        Console.ReadKey()
+
+    End Sub
+
+    Function TooLong(ByVal sentence As String) As Boolean
+
+        Return Len(sentence) > Console.WindowWidth
+
+    End Function
+
+End Module
