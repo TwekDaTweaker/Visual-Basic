30ddf89c8aaee912ddb1afa8b965d5990b51fcf2
18PY0606 <18PY0606@L2484>
2018-11-22 12:12:00 +0000

    catcher movement, won/lost/null game conditions
    need to nurf catcher and fix him going out of bounds once in a while

--- ConsoleAppDog/ConsoleAppDog/Module1.vb
+++ ConsoleAppDog/ConsoleAppDog/Module1.vb
@@ -25,10 +25,20 @@
 
     End Class
 
-    Const height As Integer = 16
-    Const width As Integer = 16
+    Enum State
+
+        null
+        won
+        lost
+
+    End Enum
+
+    Const height As Integer = 7
+    Const width As Integer = 7
 
     Sub Main()
+
+        Dim map(height, width)
 
         Dim house As New Vec2(0, 0)
         Dim DogPos As New Vec2(0, 0)
@@ -40,20 +50,19 @@
 
             While True
 
-                Dim caught As Integer = 0
-                ' 0 - null; 1 - lost; 2 - won;
+                Dim gameState As Integer = State.null
 
-                UpdateDogPos(DogPos)
-                UpdateCatchPos(DogPos, CatchPos, caught)
+                UpdateDogPos(DogPos, house, gameState)
+                UpdateCatchPos(CatchPos, DogPos, house, gameState)
 
-                If caught = 1 Then
+                If gameState = State.lost Then
 
                     Console.Clear()
                     Console.SetWindowSize(120, 30)
                     Console.WriteLine("You loose!")
                     Exit While
 
-                ElseIf caught = 2 Then
+                ElseIf gameState = State.won Then
 
                     Console.Clear()
                     Console.SetWindowSize(120, 30)
@@ -64,37 +73,44 @@
 
             End While
 
-            Console.Write(vbNewLine & "Would you like to continue playing? (y/n) ")
+            Console.Write(vbNewLine & "Would you like to continue playing? (y/n)")
+            Dim key
 
-            Dim key = Console.ReadKey()
+            Do
 
-            If key.KeyChar = "n" Then
+                Console.SetCursorPosition(42, 2)
 
-                Exit While
+                key = Console.ReadKey()
 
-            End If
+                Console.SetCursorPosition(42, 2)
+                Console.Write(" ")
+
+                If key.KeyChar = "n" Then
+
+                    Exit While
+
+                End If
+
+            Loop Until LCase(key.keyChar) = "y" Or LCase(key.keyChar) = "n"
 
         End While
 
     End Sub
 
-    Sub UpdateDogPos(ByRef DogPos As Vec2)
+    Sub UpdateDogPos(ByRef DogPos As Vec2, ByVal house As Vec2, ByRef gameState As Integer)
 
         Dim allowed As Boolean = False
 
         Do
 
-            Console.SetCursorPosition(0, 0)
+            Console.SetCursorPosition((DogPos.x * 2) + 1, DogPos.y)
 
             Dim key = Console.ReadKey()
 
-            Console.SetCursorPosition(0, 0)
-            Console.Write("|")
-
-            Console.SetCursorPosition((DogPos.x * 2) - 1, DogPos.y)
+            Console.SetCursorPosition((DogPos.x * 2) + 1, DogPos.y)
             Console.Write(" ")
 
-            Select Case key.KeyChar
+            Select Case LCase(key.KeyChar)
 
                 Case "w"
 
@@ -107,7 +123,7 @@
 
                 Case "a"
 
-                    If DogPos.x > 1 Then
+                    If DogPos.x > 0 Then
 
                         DogPos = New Vec2(DogPos.x - 1, DogPos.y)
                         allowed = True
@@ -125,7 +141,7 @@
 
                 Case "d"
 
-                    If DogPos.x < width + 1 Then
+                    If DogPos.x < width Then
 
                         DogPos = New Vec2(DogPos.x + 1, DogPos.y)
                         allowed = True
@@ -134,16 +150,62 @@
 
             End Select
 
-            Console.SetCursorPosition((DogPos.x * 2) - 1, DogPos.y)
+            If DogPos.x = house.x And DogPos.y = house.y Then
+
+                Console.SetCursorPosition((DogPos.x * 2) + 1, DogPos.y)
+                Console.Write("H")
+
+                gameState = State.won
+                Exit Sub
+
+            End If
+
+            Console.SetCursorPosition((DogPos.x * 2) + 1, DogPos.y)
             Console.Write("D")
 
         Loop Until allowed
 
     End Sub
 
-    Sub UpdateCatchPos(ByRef DogPos As Vec2, ByRef CatchPos As Vec2, ByRef caught As Integer)
+    Sub UpdateCatchPos(ByRef CatchPos As Vec2, ByVal DogPos As Vec2, ByVal house As Vec2, ByRef gameState As Integer)
 
+        Console.SetCursorPosition((CatchPos.x * 2) + 1, CatchPos.y)
+        Console.Write(" ")
 
+        If Math.Abs(CatchPos.x - DogPos.x) > Math.Abs(CatchPos.y - DogPos.y) Then
+
+            If CatchPos.x - DogPos.x < 0 Then
+
+                CatchPos.x += 1
+
+            Else
+
+                CatchPos.x -= 1
+
+            End If
+
+        Else
+
+            If CatchPos.y - DogPos.y < 0 Then
+
+                CatchPos.y += 1
+
+            Else
+
+                CatchPos.y -= 1
+
+            End If
+
+        End If
+
+        Console.SetCursorPosition((CatchPos.x * 2) + 1, CatchPos.y)
+        Console.Write("C")
+
+        If CatchPos.x = DogPos.x And CatchPos.y = DogPos.y Then
+
+            gameState = State.lost
+
+        End If
 
     End Sub
 
@@ -152,12 +214,11 @@
         Console.Clear()
         Console.Write("Setting up")
 
+        'set random positions
         Randomize()
         Dim rand As New Random
 
         house = New Vec2(Int(rand.Next(1, width)), Int(rand.Next(0, height)))
-
-        Console.Write(".")
 
         Do
 
@@ -165,13 +226,15 @@
 
         Loop Until (DogPos.x - house.x) ^ 2 > 4 Or (DogPos.y - house.y) ^ 2 > 4
 
-        Console.Write(".")
-
         Do
 
             CatchPos = New Vec2(Int(rand.Next(1, width)), Int(rand.Next(0, height)))
 
-        Loop Until (DogPos.x - CatchPos.x) ^ 2 > 4 Or (DogPos.y - CatchPos.y) ^ 2 > 4
+        Loop Until Math.Abs(DogPos.x - CatchPos.x) > 2 Or Math.Abs(DogPos.y - CatchPos.y) > 2
+
+        Console.Write(".")
+
+        Console.Write(".")
 
         Console.Write(". Done!" & vbNewLine & vbNewLine & "Press any key to start.")
         Console.ReadKey()
@@ -188,23 +251,25 @@
 
         For j = 0 To height
 
+            Console.Write("|")
+
             For i = 0 To width
 
-                Console.Write("| ")
+                Console.Write(" |")
 
             Next
 
-            Console.Write("|" & vbNewLine)
+            Console.Write(vbNewLine)
 
         Next
 
-        Console.SetCursorPosition((house.x * 2) - 1, house.y)
+        Console.SetCursorPosition((house.x * 2) + 1, house.y)
         Console.Write("H")
 
-        Console.SetCursorPosition((DogPos.x * 2) - 1, DogPos.y)
+        Console.SetCursorPosition((DogPos.x * 2) + 1, DogPos.y)
         Console.Write("D")
 
-        Console.SetCursorPosition((CatchPos.x * 2) - 1, CatchPos.y)
+        Console.SetCursorPosition((CatchPos.x * 2) + 1, CatchPos.y)
         Console.Write("C")
 
     End Sub
