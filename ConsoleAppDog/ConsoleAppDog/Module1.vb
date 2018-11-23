f31117f2c90804f8620c56c81b41f9dbfe52f43a
Twek Da Tweaker <twekdatweaker@gmail.com>
2018-11-23 22:18:00 +0000

    most of the game, need to write A* algorithum

--- ConsoleAppDog/ConsoleAppDog/Module1.vb
+++ ConsoleAppDog/ConsoleAppDog/Module1.vb
@@ -2,7 +2,8 @@
 
     Public Class Vec2
 
-        Public x, y As Integer
+        Public x As Integer = 0
+        Public y As Integer = 0
 
         Public Sub New(ByVal in_x As Integer, ByVal in_y As Integer)
 
@@ -10,6 +11,18 @@
             y = in_y
 
         End Sub
+
+        Public Shared Operator =(ByVal V1 As Vec2, ByVal V2 As Vec2) As Boolean
+
+            Return V1.x = V2.x And V1.y = V2.y
+
+        End Operator
+
+        Public Shared Operator <>(ByVal V1 As Vec2, ByVal V2 As Vec2) As Boolean
+
+            Return V1.x <> V2.x Or V1.y <> V2.y
+
+        End Operator
 
         Public Shared Operator +(ByVal V1 As Vec2, ByVal V2 As Vec2) As Vec2
 
@@ -25,158 +38,305 @@
 
     End Class
 
+    Enum state
+
+        null
+        won
+        lost
+
+    End Enum
+
+    Enum obsticle
+
+        null
+        bush
+        tree
+
+    End Enum
+
     Const height As Integer = 16
     Const width As Integer = 16
 
-    Sub Main()
+    Structure Objects
 
+        Dim map(,) As Integer
         Dim house As Vec2
         Dim DogPos As Vec2
         Dim CatchPos As Vec2
 
+    End Structure
+
+    Sub Main()
+
+        Dim obj As Objects
+
         While True
 
-            Setup(house, DogPos, CatchPos)
+            Dim gameState = state.null
+            Setup(obj)
 
             While True
 
-                Dim caught As Boolean = False
+                UpdateDogPos(obj, gameState)
+                UpdateCatchPos(obj, gameState)
 
-                UpdateDogPos(DogPos)
-                UpdateCatchPos(DogPos, CatchPos, caught)
-
-                If caught Then
+                If gameState = state.lost Then
 
                     Console.Clear()
                     Console.SetWindowSize(120, 30)
-                    Console.WriteLine("You loose!")
+                    Console.WriteLine("You Lost!")
+                    Exit While
+
+                ElseIf gameState = state.won Then
+
+                    Console.Clear()
+                    Console.SetWindowSize(120, 30)
+                    Console.WriteLine("You Won!")
                     Exit While
 
                 End If
 
             End While
 
-            Console.Write(vbNewLine & "Would you like to continue playing? (y/n)")
+            Console.Write(vbNewLine & "Would you like to continue playing? (y/n) ")
 
-            Dim key = Console.ReadKey()
+            Dim key
 
-            If key.KeyChar = "n" Then
+            Do
 
-                Exit While
+                key = Console.ReadKey
 
-            End If
+                Console.SetCursorPosition(42, 2)
+                Console.Write(" ")
+                Console.SetCursorPosition(42, 2)
+
+                If LCase(key.Keychar) = "n" Then
+
+                    Exit While
+
+                End If
+
+            Loop Until LCase(key.Keychar) = "n" Or LCase(key.Keychar) = "y"
 
         End While
 
-
-        Console.ReadKey()
-
     End Sub
 
-    Sub UpdateDogPos(ByRef DogPos As Vec2)
+    Sub UpdateDogPos(ByRef obj As Objects, ByRef gameState As Integer)
 
         Dim allowed As Boolean = False
 
         Do
 
-            Console.SetCursorPosition(0, 0)
-
+            Console.ForegroundColor = ConsoleColor.Green
+            Console.SetCursorPosition(obj.house.x * 2 + 1, obj.house.y)
             Dim key = Console.ReadKey()
+            Console.SetCursorPosition(obj.house.x * 2 + 1, obj.house.y)
+            Console.Write("H")
+            Console.ResetColor()
 
-            Console.SetCursorPosition(0, 0)
-            Console.Write("|")
+            Console.SetCursorPosition((obj.DogPos.x * 2) + 1, obj.DogPos.y)
 
-            Console.SetCursorPosition((DogPos.x * 2) - 1, DogPos.y)
-            Console.Write(" ")
+            If obj.map(obj.DogPos.y, obj.DogPos.x) = obsticle.bush Then
 
-            Select Case key.KeyChar
+                Console.Write("#")
+
+            Else
+
+                Console.Write(" ")
+
+            End If
+
+            Select Case LCase(key.KeyChar)
 
                 Case "w"
 
-                    If DogPos.y > 0 Then
+                    If obj.DogPos.y > 0 Then
 
-                        DogPos = New Vec2(DogPos.x, DogPos.y - 1)
-                        allowed = True
+                        If obj.map(obj.DogPos.y - 1, obj.DogPos.x) <> obsticle.tree Then
+
+                            obj.DogPos.y += -1
+                            allowed = True
+
+                        End If
 
                     End If
 
                 Case "a"
 
-                    If DogPos.x > 1 Then
+                    If obj.DogPos.x > 0 Then
 
-                        DogPos = New Vec2(DogPos.x - 1, DogPos.y)
-                        allowed = True
+                        If obj.map(obj.DogPos.y, obj.DogPos.x - 1) <> obsticle.tree Then
+
+                            obj.DogPos.x += -1
+                            allowed = True
+
+                        End If
 
                     End If
 
                 Case "s"
 
-                    If DogPos.y < height Then
+                    If obj.DogPos.y < height Then
 
-                        DogPos = New Vec2(DogPos.x, DogPos.y + 1)
-                        allowed = True
+                        If obj.map(obj.DogPos.y + 1, obj.DogPos.x) <> obsticle.tree Then
+
+                            obj.DogPos.y += 1
+                            allowed = True
+
+                        End If
 
                     End If
 
                 Case "d"
 
-                    If DogPos.x < width + 1 Then
+                    If obj.DogPos.x < width Then
 
-                        DogPos = New Vec2(DogPos.x + 1, DogPos.y)
-                        allowed = True
+                        If obj.map(obj.DogPos.y, obj.DogPos.x + 1) <> obsticle.tree Then
+
+                            obj.DogPos.x += 1
+                            allowed = True
+
+                        End If
 
                     End If
 
             End Select
 
-            Console.SetCursorPosition((DogPos.x * 2) - 1, DogPos.y)
-            Console.Write("D")
+            If obj.map(obj.DogPos.y, obj.DogPos.x) = obsticle.null Then
+
+                Console.ForegroundColor = ConsoleColor.Blue
+                Console.SetCursorPosition((obj.DogPos.x * 2) + 1, obj.DogPos.y)
+                Console.Write("D")
+                Console.ResetColor()
+
+            End If
 
         Loop Until allowed
 
+        If obj.DogPos = obj.house Then
+
+            gameState = state.won
+
+        End If
+
     End Sub
 
-    Sub UpdateCatchPos(ByRef DogPos As Vec2, ByRef CatchPos As Vec2, ByRef caught As Boolean)
+    Sub UpdateCatchPos(ByRef obj As Objects, ByRef gameState As Integer)
 
+        Console.SetCursorPosition((obj.CatchPos.x * 2) + 1, obj.CatchPos.y)
+        Console.Write(" ")
 
+        'insert A* algorithum here
+
+        Console.ForegroundColor = ConsoleColor.Red
+        Console.SetCursorPosition((obj.CatchPos.x * 2) + 1, obj.CatchPos.y)
+        Console.Write("C")
+        Console.ResetColor()
+
+        If obj.CatchPos = obj.DogPos And gameState = state.null Then
+
+            gameState = state.lost
+
+        End If
 
     End Sub
 
-    Sub Setup(ByRef house As Vec2, ByRef DogPos As Vec2, ByRef CatchPos As Vec2)
+    Sub Setup(ByRef obj As Objects)
 
         Console.Clear()
         Console.Write("Setting up")
 
+        'setting up obsticles
         Randomize()
         Dim rand As New Random
 
-        house = New Vec2(Int(rand.Next(1, width)), Int(rand.Next(0, height)))
+        Dim temp(height + 1, width + 1) As Integer
+
+        For i = 0 To height
+
+            For l = 0 To width
+
+                If Int(rand.Next(0, 1000)) < 50 Then
+
+                    temp(i, l) = obsticle.bush
+
+                ElseIf Int(rand.Next(0, 1000)) < 80 Then
+
+                    temp(i, l) = obsticle.tree
+
+                End If
+
+            Next
+
+        Next
+
+        obj.map = temp
 
         Console.Write(".")
 
+        'setting up game objects
         Do
 
-            DogPos = New Vec2(Int(rand.Next(1, width)), Int(rand.Next(0, height)))
+            obj.house = New Vec2(Int(rand.Next(0, width)), Int(rand.Next(0, height)))
 
-        Loop Until (DogPos.x - house.x) ^ 2 > 4 Or (DogPos.y - house.y) ^ 2 > 4
+        Loop Until obj.map(obj.house.y, obj.house.x) = obsticle.null
+
+        Do
+
+            obj.DogPos = New Vec2(Int(rand.Next(0, width)), Int(rand.Next(0, height)))
+
+        Loop Until (Math.Abs(obj.DogPos.x - obj.house.x) > (width / 2) - 1 Or
+                    Math.Abs(obj.DogPos.y - obj.house.y) > (height / 2) - 1) And
+                    obj.map(obj.DogPos.y, obj.DogPos.x) = obsticle.null
+
+        Do
+
+            obj.CatchPos = New Vec2(Int(rand.Next(0, width)), Int(rand.Next(0, height)))
+
+        Loop Until (Math.Abs(obj.DogPos.x - obj.CatchPos.x) > 2 Or
+                    Math.Abs(obj.DogPos.y - obj.CatchPos.y) > 2) And
+                    obj.CatchPos <> obj.house And
+                    obj.map(obj.CatchPos.y, obj.CatchPos.x) = obsticle.null
+
+        obj.map(obj.house.y, obj.house.x) = obsticle.bush
 
         Console.Write(".")
 
-        Do
+        'setting up pathfinding
 
-            CatchPos = New Vec2(Int(rand.Next(1, width)), Int(rand.Next(0, height)))
 
-        Loop Until (DogPos.x - CatchPos.x) ^ 2 > 4 Or (DogPos.y - CatchPos.y) ^ 2 > 4
+        Console.Write(".")
 
-        Console.Write(". Done!" & vbNewLine & vbNewLine & "Press any key to start.")
+        Console.WriteLine(" Done!" & vbNewLine)
+
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.Write("H")
+        Console.ResetColor()
+        Console.WriteLine(" - House")
+        Console.ForegroundColor = ConsoleColor.Blue
+        Console.Write("D")
+        Console.ResetColor()
+        Console.WriteLine(" - Dog")
+        Console.ForegroundColor = ConsoleColor.Red
+        Console.Write("C")
+        Console.ResetColor()
+        Console.WriteLine(" - Catcher" & vbNewLine)
+
+        Console.WriteLine("# - The dog can pass through bushes, but the catcher can't.")
+        Console.WriteLine("T - Neither the dog or the catcher can pass through trees." & vbNewLine)
+
+        Console.WriteLine("Use 'w', 'a', 's' and 'd' to move.")
+
+        Console.Write(vbNewLine & "Press any key to start.")
         Console.ReadKey()
 
-        Console.SetWindowSize(width * 2 + 7, height + 3)
-        DrawGrid(house, DogPos, CatchPos)
+        Console.SetWindowSize(width * 2 + 3, height + 2)
+        DrawGrid(obj)
 
     End Sub
 
-    Sub DrawGrid(ByVal house As Vec2, ByVal DogPos As Vec2, ByVal CatchPos As Vec2)
+    Sub DrawGrid(ByVal obj As Objects)
 
         Console.Clear()
         Console.CursorVisible = False
@@ -185,7 +345,23 @@
 
             For i = 0 To width
 
-                Console.Write("| ")
+                Console.Write("|")
+
+                Select Case obj.map(j, i)
+
+                    Case obsticle.bush
+
+                        Console.Write("#")
+
+                    Case obsticle.tree
+
+                        Console.Write("T")
+
+                    Case Else
+
+                        Console.Write(" ")
+
+                End Select
 
             Next
 
@@ -193,14 +369,19 @@
 
         Next
 
-        Console.SetCursorPosition((house.x * 2) - 1, house.y)
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.SetCursorPosition((obj.house.x * 2) + 1, obj.house.y)
         Console.Write("H")
 
-        Console.SetCursorPosition((DogPos.x * 2) - 1, DogPos.y)
+        Console.ForegroundColor = ConsoleColor.Blue
+        Console.SetCursorPosition((obj.DogPos.x * 2) + 1, obj.DogPos.y)
         Console.Write("D")
 
-        Console.SetCursorPosition((CatchPos.x * 2) - 1, CatchPos.y)
+        Console.ForegroundColor = ConsoleColor.Red
+        Console.SetCursorPosition((obj.CatchPos.x * 2) + 1, obj.CatchPos.y)
         Console.Write("C")
+
+        Console.ResetColor()
 
     End Sub
 
