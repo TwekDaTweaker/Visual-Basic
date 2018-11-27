fbfac6caf55f4b59cc113561a12ea170aca768a5
18PY0606 <18PY0606@L2238>
2018-11-27 15:12:20 +0000

    swap

---
+++ ConsoleAppByRef/ConsoleAppByRef/Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim x As Integer = 5
+        Dim y As Integer = 3
+
+        Console.WriteLine(x & " " & y)
+
+        swap(x, y)
+
+        Console.WriteLine(x & " " & y)
+
+        Console.ReadKey()
+
+    End Sub
+
+    Sub swap(ByRef x As Integer, ByRef y As Integer)
+
+        x += y
+
+        y = x - y
+
+        x -= y
+
+    End Sub
+
+End Module
