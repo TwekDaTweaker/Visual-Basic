eac03768816c01b2c9c5670710f325ef4e45b826
18PY0606 <18PY0606@L2238>
2018-12-07 10:54:03 +0000

    database

---
+++ ConsoleAppFileStream/ConsoleAppFileStream/Module1.vb
@@ -0,0 +1,25 @@
+﻿Imports System.IO
+
+Module Module1
+
+    Sub Main()
+
+        Dim filename As String = Directory.GetCurrentDirectory & "..\..\..\database.txt"
+        Dim line As String
+
+        Using Reader As StreamReader = New StreamReader(filename)
+
+            Do Until Reader.EndOfStream
+
+                line = Reader.ReadLine()
+                Console.WriteLine(line)
+
+            Loop
+
+        End Using
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ ConsoleAppFileStream/ConsoleAppFileStream/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ConsoleAppFileStream")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ConsoleAppFileStream")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("18c61a38-3896-48e3-bc34-b9ea9210d615")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
