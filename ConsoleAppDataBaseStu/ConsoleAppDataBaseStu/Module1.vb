5690d429875c72742e58459dbe2bf593319564de
18PY0606 <18PY0606@L2238>
2018-12-11 15:58:12 +0000

    database almost done

--- ConsoleAppDataBaseStu/ConsoleAppDataBaseStu/Module1.vb
+++ ConsoleAppDataBaseStu/ConsoleAppDataBaseStu/Module1.vb
@@ -42,7 +42,7 @@ Module Module1
 
         Sub Show()
 
-
+            Console.Write(vbNewLine & vbNewLine & "nope")
 
         End Sub
 
@@ -51,26 +51,48 @@ Module Module1
     Sub Main()
 
         Dim record As New List(Of Student)
-
         Dim filename As String = Directory.GetCurrentDirectory & "..\..\..\database.txt"
         Dim line As String
 
-        Using Writer As StreamWriter = New StreamWriter(filename)
-
-            For i = 0 To 9
-
-                Writer.WriteLine("This is line: " & i + 1)
-
-            Next
-
-        End Using
-
         Using Reader As StreamReader = New StreamReader(filename)
+
+            Dim current As Student
 
             Do Until Reader.EndOfStream
 
                 line = Reader.ReadLine()
-                Console.WriteLine(line)
+
+                If line(0) = "#" Then
+
+                    Select Case line(1)
+
+                        Case "1"
+
+                            current.title = CByte(Mid(line, 2, line.Length))
+
+                        Case "2"
+
+                            current.firstName = Mid(line, 2, line.Length - 1)
+
+                        Case "3"
+
+                            current.lastName = Mid(line, 2, line.Length - 1)
+
+                        Case "4"
+
+                            current.age = CInt(Mid(line, 2, line.Length - 1))
+
+                        Case "5"
+
+                            current.favSubject = Mid(line, 2, line.Length - 1)
+
+                        Case Else
+
+                            record.Add(current)
+
+                    End Select
+
+                End If
 
             Loop
 
@@ -114,7 +136,7 @@ Module Module1
 
                         Next
 
-                        For i = 0 To found.Count
+                        For i = 0 To found.Count - 1
 
                             found(i).Show()
 
@@ -145,6 +167,21 @@ Module Module1
             End Select
 
         End While
+
+        Using Writer As StreamWriter = New StreamWriter(filename)
+
+            For i = 0 To record.Count - 1
+
+                Writer.WriteLine("#1" & record(i).title)
+                Writer.WriteLine("#2" & record(i).firstName)
+                Writer.WriteLine("#3" & record(i).lastName)
+                Writer.WriteLine("#4" & record(i).age)
+                Writer.WriteLine("#5" & record(i).favSubject)
+                Writer.WriteLine("#   ")
+
+            Next
+
+        End Using
 
     End Sub
 
