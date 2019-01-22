c98ac0ccefe8ff03abd50c77169498b185aa0e8f
18PY0606 <18PY0606@L2109>
2019-01-22 12:40:58 +0000

    Isogram

---
+++ ConsoleAppIsogram/ConsoleAppIsogram/Module1.vb
@@ -0,0 +1,69 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim str As String = Console.ReadLine()
+
+        Console.WriteLine(Isogram(str))
+
+        Console.ReadKey(True)
+
+    End Sub
+
+    Function Isogram(ByVal str As String) As Boolean
+
+        Dim lett As New List(Of String)
+
+        For i = 0 To str.Length - 1
+
+            If Asc(LCase(str(i))) >= 97 And Asc(LCase(str(i))) <= 122 Then
+
+                Dim found As Boolean = False
+
+                For Each item In lett
+
+                    If LCase(str(i)) = item(0) Then
+
+                        item &= LCase(str(i))
+                        found = True
+                        Exit For
+
+                    End If
+
+                Next
+
+                If Not found Then
+
+                    lett.Add(LCase(str(i)))
+
+                End If
+
+            End If
+
+        Next
+
+        If lett.Count > 0 Then
+
+            Dim def As Integer = lett(0).Length()
+
+            For i = 1 To lett.Count - 1
+
+                If lett(i).Length() <> def Then
+
+                    Return False
+
+                End If
+
+            Next
+
+            Return True
+
+        Else
+
+            Return False
+
+        End If
+
+    End Function
+
+End Module
---
+++ ConsoleAppIsogram/ConsoleAppIsogram/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ConsoleAppIsogram")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ConsoleAppIsogram")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5135390d-a276-4d8b-885d-92ffd8c4f63a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
