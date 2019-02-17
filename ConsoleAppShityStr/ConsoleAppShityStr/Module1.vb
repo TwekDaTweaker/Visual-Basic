43657471b2acc07a50fbb28593c956d57a242f99
None <none>
2019-02-17 21:32:14 +0000

    Shity string - function

--- ConsoleAppShityStr/ConsoleAppShityStr/Module1.vb
+++ ConsoleAppShityStr/ConsoleAppShityStr/Module1.vb
@@ -34,9 +34,16 @@
         Next
 
         Console.WriteLine(vbNewLine & "E: x=" & E(0) & ", y=" & E(1) & vbNewLine & "M: x=" & M(0) & ", y=" & M(1))
+        Console.WriteLine("M is " & HowFar(E, M) & " moves away from E.")
 
         Console.ReadKey(True)
 
     End Sub
+
+    Function HowFar(ByVal E() As Integer, ByVal M() As Integer) As Integer
+
+        Return Math.Abs(E(0) - M(0)) + Math.Abs(E(1) - M(1))
+
+    End Function
 
 End Module
