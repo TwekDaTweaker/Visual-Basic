a39c97d9ec058c5f54faa4406e64676e5b03847b
None <none>
2019-02-18 15:05:00 +0000

    Sum of numbers done

---
+++ ConsoleAppSumOfNumbers/ConsoleAppSumOfNumbers/Module1.vb
@@ -0,0 +1,88 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim grid(5, 5) As Integer
+
+        Init(grid)
+
+        Dim hRow As Integer = 0
+        For y = 0 To 4
+            If grid(5, y) > grid(5, hRow) Then
+                hRow = y
+            End If
+        Next
+
+        Dim hCol As Integer = 0
+        For x = 0 To 4
+            If grid(x, 5) > grid(hCol, 5) Then
+                hCol = x
+            End If
+        Next
+
+        Draw(grid, hRow, hCol)
+
+        Console.ReadKey(True)
+
+    End Sub
+
+    Sub Init(ByRef grid(,) As Integer)
+
+        Randomize()
+        Dim rand As New Random
+        For y = 0 To 4
+            For x = 0 To 4
+                grid(x, y) = rand.Next(10, 99)
+            Next
+        Next
+
+        For y = 0 To 4
+            Dim sumx As Integer
+            For x = 0 To 4
+                sumx += grid(x, y)
+            Next
+            grid(5, y) = sumx
+            sumx = 0
+        Next
+
+        For x = 0 To 4
+            Dim sumy As Integer
+            For y = 0 To 4
+                sumy += grid(x, y)
+            Next
+            grid(x, 5) = sumy
+            sumy = 0
+        Next
+
+    End Sub
+
+    Sub Draw(ByVal grid(,) As Integer, ByVal hRow As Integer, ByVal hCol As Integer)
+
+        Console.ForegroundColor = ConsoleColor.White
+        For y = 0 To 5
+            For x = 0 To 5
+                Console.Write("|")
+                If y = hRow Then
+                    Console.ForegroundColor = ConsoleColor.Cyan
+                End If
+                If x = hCol Then
+                    Console.ForegroundColor = ConsoleColor.Red
+                End If
+                If y = hRow And x = hCol Then
+                    Console.ForegroundColor = ConsoleColor.Yellow
+                End If
+                Console.Write(grid(x, y) & " ")
+                If grid(x, y) < 100 Then
+                    Console.Write(" ")
+                    If grid(x, y) < 10 Then
+                        Console.Write(" ")
+                    End If
+                End If
+                Console.ForegroundColor = ConsoleColor.White
+            Next
+            Console.WriteLine("|")
+        Next
+
+    End Sub
+
+End Module
---
+++ ConsoleAppSumOfNumbers/ConsoleAppSumOfNumbers/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ConsoleAppSumOfNumbers")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ConsoleAppSumOfNumbers")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("644898eb-421c-4123-b4f2-9ffc9b48471e")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
