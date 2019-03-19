29e36aed42b69d25dda5915ba407b6a8e415eab5
18py0606 <18py0606@L2243>
2019-03-19 16:00:29 +0000

    Inheritance start

---
+++ Inheritance/Inheritance/Account.vb
@@ -0,0 +1,51 @@
+﻿Public Class Account
+
+    Private Balance As Integer
+    Private OverDraftLimit As Integer
+    Private owner As Customer
+
+    Public Sub New(ByVal startBal As Integer, ByVal premium As Boolean)
+
+        Me.New(startBal)
+        If premium Then OverDraftLimit = 1500
+
+    End Sub
+
+    Public Sub New()
+
+        Balance = 0
+        OverDraftLimit = 0
+
+    End Sub
+
+    Public Sub New(ByVal startBalance As Integer)
+
+        Balance = startBalance
+        OverDraftLimit = 0
+
+    End Sub
+
+    Public Sub MakeDeposit(ByVal Amount As Integer)
+
+        Balance += Amount
+
+    End Sub
+
+    Public Function GetBalance() As Integer
+
+        Return Balance
+
+    End Function
+
+    Public Sub MakeWithdrawal(ByVal Amount As Integer)
+
+        If Balance + OverDraftLimit >= Amount Then
+            Balance -= Amount
+        Else
+            Throw New ArgumentException("Insufficient funds.")
+        End If
+
+    End Sub
+
+End Class
+
---
+++ Inheritance/Inheritance/Customer.vb
@@ -0,0 +1,29 @@
+﻿Public Class Customer
+
+    Private acc As Account
+    Private name As String
+    Private premium As Boolean
+
+    Public Sub New(ByVal name As String, ByVal startBal As Integer, ByVal hasPremium As Boolean)
+
+        Me.name = name
+        Me.premium = hasPremium
+        Me.acc = New Account(startBal, premium, Me)
+
+    End Sub
+
+    Public Sub New()
+        Me.name = "Steve"
+        Me.premium = False
+        Me.acc = New Account(0)
+    End Sub
+
+    Public Function IsPremium() As Boolean
+        Return premium
+    End Function
+
+    Public Function GetName() As String
+        Return name
+    End Function
+
+End Class
