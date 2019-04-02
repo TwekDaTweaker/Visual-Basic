88211f7d4e7565d331ee60258038aa1a1fd748d2
18py0606 <18py0606@L2243>
2019-04-02 15:55:13 +0100

    snek refactor and Stack

---
+++ Stack/Stack/Stack.vb
@@ -0,0 +1,35 @@
+﻿Public Class Stack
+
+    Private top As Integer = -1
+    Private arr() As Integer
+
+    Public Sub New(ByVal size As Integer)
+        Dim arr(size) As Integer
+        Me.arr = arr
+    End Sub
+
+    Public Function Peek() As Integer
+        Return arr(top)
+    End Function
+
+    Public Function Pop() As Integer
+        top -= 1
+        Return arr(top + 1)
+    End Function
+
+    Public Sub Push(ByVal val As Integer)
+        top += 1
+        arr(top) = val
+    End Sub
+
+    Public Function IsEmpty() As Boolean
+        If top = -1 Then Return True
+        Return False
+    End Function
+
+    Public Function IsFull() As Boolean
+        If top = arr.Length() Then Return True
+        Return False
+    End Function
+
+End Class
--- snek/snek/Module1.vb
+++ snek/snek/Module1.vb
@@ -2,21 +2,58 @@
 
     Sub Main()
 
-        Dim pos As New Vec2(3, 4)
-        Dim snake As New Head(pos)
-        Dim parts As New List(Of Part)
-        Dim food As New List(Of Vec2)
         Dim ft As New FrameTimer
-        Dim dt As Decimal
+        Dim dt As Decimal = ft.Mark()
+        Dim vel As New Vec2(1, 0)
+        Dim speed As Decimal
+        Dim elapsedTime As Decimal
+        Dim snek As New List(Of Vec2)
+        snek.Add(New Vec2(4, 4))
+        snek.Add(New Vec2(3, 4))
+        snek.Add(New Vec2(2, 4))
+        snek.Add(New Vec2(1, 4))
 
-        parts.Add(snake)
+        While (True)
+            dt = ft.Mark
 
-        While True
-            dt = ft.Mark()
-            snake.Update(food, parts, dt)
+            Dim update = Sub(ByRef vel As Vec2, ByVal speed As Decimal, ByRef snek As List(Of Vec2))
+                             If Console.KeyAvailable = True Then
+                                 Dim key = Console.ReadKey(True)
+                                 Select Case LCase(key.KeyChar())
+                                     Case "w"
+                                         vel.y = -1
+                                         vel.x = 0
+                                     Case "a"
+                                         vel.x = -1
+                                         vel.y = 0
+                                     Case "s"
+                                         vel.y = +1
+                                         vel.x = 0
+                                     Case "d"
+                                         vel.x = +1
+                                         vel.y = 0
+                                 End Select
+                             End If
+                             If elapsedTime > (1 / speed) Then
+                                 Draw(snek(0), True)
+                                 snek(0) += vel
+                                 elapsedTime = 0
+                             End If
+                             elapsedTime += dt
+                         End Sub
+            update()
+
         End While
 
+    End Sub
 
+    Sub Draw(ByVal pos As Vec2, ByVal isWhite As Boolean)
+        Console.SetCursorPosition(pos.x * 2, pos.y)
+        If Not isWhite Then
+            Console.Write("  ")
+            Exit Sub
+        End If
+        Console.Write(Asc(219) & Asc(219))
     End Sub
 
 End Module
