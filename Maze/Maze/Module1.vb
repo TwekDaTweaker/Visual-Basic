5f1c29f59a859a69a9f37932dac6963589762533
18PY0606 <18PY0606@L2474>
2019-04-12 15:37:42 +0100

    maze is complete

--- Maze/Maze/Module1.vb
+++ Maze/Maze/Module1.vb
@@ -2,8 +2,10 @@
 
     Sub Main()
 
-        Randomize()
-        Dim width As Integer = 41
+        While True
+
+            Randomize()
+            Dim width As Integer = 41
         Dim height As Integer = 41
         Console.SetWindowSize(width * 2, height)
         Dim map(height, width) As Boolean
@@ -13,26 +15,28 @@
         Dim ft As New FrameTimer
         Dim dt As Decimal = ft.Mark()
 
+        'calculating start position
         Dim rand As New Random()
         Dim start As New Vec2(1, rand.Next(1, height))
         While start.y Mod 2 = 0
             start.y = rand.Next(0, height)
         End While
 
-        'true means it was explored
+        'true means no wall
         map(start.y, start.x) = True
         map(start.y, start.x - 1) = True
         For j = 0 To height - 1
-            For o = 0 To width - 1
-                If Not map(j, o) Then
-                    Draw(New Vec2(o, j), ConsoleColor.White)
-                Else
-                    Draw(New Vec2(o, j), ConsoleColor.Black)
-                End If
+            For i = 0 To width - 1
+                Draw(New Vec2(j, i), ConsoleColor.White)
             Next
         Next
+        Draw(start, ConsoleColor.Gray)
+        Draw(New Vec2(start.x - 1, start.y), ConsoleColor.Gray)
+
+        'calling recursive function to create a maze
         maze(map, start, width, height)
 
+        'finding a suitable position for the end of the maze
         Dim finish As New Vec2(width - 1, rand.Next(1, height))
         While finish.y Mod 2 = 0 And Not map(finish.y, finish.x - 1)
             finish.y = rand.Next(1, height)
@@ -40,42 +44,51 @@
         map(finish.y, finish.x) = True
         start = New Vec2(start.x - 1, start.y)
 
+        'destroying walls to make the maze more treversable
         Dim walls As New List(Of Vec2)
         For j = 1 To width - 2
             For i = 1 To height - 2
-                If Not map(j, i) And
-                    ((map(j + 1, i) Or map(j - 1, i)) And (map(j, i + 1) Or map(j, i - 1))) Then
-                    walls.Add(New Vec2(i, j))
-                End If
-            Next
-        Next
-        For Each wall In walls
-            If rand.Next(0, 101) <= 15 Then
-                map(wall.y, wall.x) = True
-                Draw(wall, ConsoleColor.Black)
-            End If
-        Next
-
-        For j = 0 To height - 1
-            For o = 0 To width - 1
-                If Not map(j, o) Then
-                    Draw(New Vec2(o, j), ConsoleColor.White)
-                Else
-                    Draw(New Vec2(o, j), ConsoleColor.Black)
+                Dim U = Not map(j - 1, i)
+                Dim D = Not map(j + 1, i)
+                Dim R = Not map(j, i + 1)
+                Dim L = Not map(j, i - 1)
+                If Not map(j, i) And ((U And D And Not (L Or R)) Or (Not (U Or D) And L And R)) Then    ' ? ? | ?#?
+                    If rand.Next(0, 101) <= 7 Then                                                      ' #0# |  0
+                        walls.Add(New Vec2(i, j))                                                       ' ? ? | ?#?
+                        map(j, i) = True
+                        Draw(walls(walls.Count - 1), ConsoleColor.DarkGray)
+                        Threading.Thread.Sleep(10)
+                    End If
                 End If
             Next
         Next
 
-        Draw(finish, ConsoleColor.Black)
+        'For j = 0 To height - 1
+        '    For o = 0 To width - 1
+        '        If Not map(j, o) Then
+        '            Draw(New Vec2(o, j), ConsoleColor.White)
+        '        Else
+        '            Draw(New Vec2(o, j), ConsoleColor.Black)
+        '        End If
+        '    Next
+        'Next
 
+        Draw(finish, ConsoleColor.DarkGray)
+
+        'pathfinding using A* algorithm
         Dim path As List(Of Vec2) = AStar(map, width, height, start, finish)
 
+        'end animation
+        Draw(start, ConsoleColor.Green)
+        path.Reverse()
         For Each point In path
             Draw(point, ConsoleColor.Green)
+            Threading.Thread.Sleep(15)
         Next
 
-        Console.ReadKey()
+            Threading.Thread.Sleep(1500)
 
+        End While
     End Sub
 
     Sub maze(ByRef map(,) As Boolean, ByVal pos As Vec2, ByVal width As Integer, ByVal height As Integer)
@@ -93,7 +106,7 @@
             temp = New Vec2(randDirections(x))
             randDirections(x) = New Vec2(randDirections(i))
             randDirections(i) = temp
-            Threading.Thread.Sleep(5)
+            Threading.Thread.Sleep(7)
         Next
 
         For i = 0 To randDirections.Length - 1
@@ -103,9 +116,11 @@
                     If Not map(pos.y - 2, pos.x) Then
                         map(pos.y - 2, pos.x) = True
                         map(pos.y - 1, pos.x) = True
-                        Draw(New Vec2(pos.x, pos.y - 2), ConsoleColor.Black)
-                        Draw(New Vec2(pos.x, pos.y - 1), ConsoleColor.Black)
+                        Draw(New Vec2(pos.x, pos.y - 2), ConsoleColor.Gray)
+                        Draw(New Vec2(pos.x, pos.y - 1), ConsoleColor.Gray)
                         maze(map, New Vec2(pos.x, pos.y - 2), width, height)
+                        Draw(New Vec2(pos.x, pos.y - 2), ConsoleColor.DarkGray)
+                        Draw(New Vec2(pos.x, pos.y - 1), ConsoleColor.DarkGray)
                     Else
                         Continue For
                     End If
@@ -114,9 +129,11 @@
                     If Not map(pos.y + 2, pos.x) Then
                         map(pos.y + 2, pos.x) = True
                         map(pos.y + 1, pos.x) = True
-                        Draw(New Vec2(pos.x, pos.y + 2), ConsoleColor.Black)
-                        Draw(New Vec2(pos.x, pos.y + 1), ConsoleColor.Black)
+                        Draw(New Vec2(pos.x, pos.y + 2), ConsoleColor.Gray)
+                        Draw(New Vec2(pos.x, pos.y + 1), ConsoleColor.Gray)
                         maze(map, New Vec2(pos.x, pos.y + 2), width, height)
+                        Draw(New Vec2(pos.x, pos.y + 2), ConsoleColor.DarkGray)
+                        Draw(New Vec2(pos.x, pos.y + 1), ConsoleColor.DarkGray)
                     Else
                         Continue For
                     End If
@@ -125,9 +142,11 @@
                     If Not map(pos.y, pos.x - 2) Then
                         map(pos.y, pos.x - 2) = True
                         map(pos.y, pos.x - 1) = True
-                        Draw(New Vec2(pos.x - 2, pos.y), ConsoleColor.Black)
-                        Draw(New Vec2(pos.x - 1, pos.y), ConsoleColor.Black)
+                        Draw(New Vec2(pos.x - 2, pos.y), ConsoleColor.Gray)
+                        Draw(New Vec2(pos.x - 1, pos.y), ConsoleColor.Gray)
                         maze(map, New Vec2(pos.x - 2, pos.y), width, height)
+                        Draw(New Vec2(pos.x - 2, pos.y), ConsoleColor.DarkGray)
+                        Draw(New Vec2(pos.x - 1, pos.y), ConsoleColor.DarkGray)
                     Else
                         Continue For
                     End If
@@ -136,14 +155,18 @@
                     If Not map(pos.y, pos.x + 2) Then
                         map(pos.y, pos.x + 2) = True
                         map(pos.y, pos.x + 1) = True
-                        Draw(New Vec2(pos.x + 2, pos.y), ConsoleColor.Black)
-                        Draw(New Vec2(pos.x + 1, pos.y), ConsoleColor.Black)
+                        Draw(New Vec2(pos.x + 2, pos.y), ConsoleColor.Gray)
+                        Draw(New Vec2(pos.x + 1, pos.y), ConsoleColor.Gray)
                         maze(map, New Vec2(pos.x + 2, pos.y), width, height)
+                        Draw(New Vec2(pos.x + 2, pos.y), ConsoleColor.DarkGray)
+                        Draw(New Vec2(pos.x + 1, pos.y), ConsoleColor.DarkGray)
                     Else
                         Continue For
                     End If
             End Select
         Next
+
+        Threading.Thread.Sleep(5)
 
     End Sub
 
@@ -216,21 +239,19 @@
 
             'end condition, returns path
             If current.pos = finish Then
-
                 Dim temp As Node = current
                 path.Add(temp.pos)
                 While temp.g > 1
                     path.Add(temp.prev.pos)
                     temp = temp.prev
                 End While
-
                 Return path
-
             End If
 
             'moves the examined node to the closed set
             openSet.Remove(current)
             closedSet.Add(current)
+            Draw(current.pos, ConsoleColor.DarkBlue)
 
             neighbors = current.neighbors
 
@@ -247,7 +268,9 @@
 
                     If Not openSet.Contains(neighbor) Then
 
+                        'adds suitable neighbor to the open set
                         openSet.Add(neighbor)
+                        Draw(neighbor.pos, ConsoleColor.Blue)
 
                     ElseIf tempG >= neighbor.g Then
 
@@ -263,12 +286,15 @@
                     neighbor.prev = current
 
                 End If
+
             Next
+
+            Threading.Thread.Sleep(30)
 
         End While
 
         'unable to find a path
-        'Throw New Exception
+        Throw New Exception
 
     End Function
 
