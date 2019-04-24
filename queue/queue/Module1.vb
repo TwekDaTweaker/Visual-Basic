d4bd28669ce7917bf16b18df9d0be43326eb056f
18py0606 <18py0606@L2243>
2019-04-24 14:14:39 +0100

    shtif

---
+++ queue/queue/Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Dim max As Integer = 0
+
+    Sub Main()
+
+        fib(Console.ReadLine())
+        Console.ReadKey(True)
+
+    End Sub
+
+    Sub fib(ByVal x As Integer)
+        If x > 0 Then
+            x -= 1
+            fib(x)
+            If x > max Then
+                Console.WriteLine(x)
+                max = x
+            End If
+            x -= 1
+            fib(x)
+        End If
+    End Sub
+
+End Module
---
+++ queue/queue/queue.vb
@@ -0,0 +1,29 @@
+﻿Public Class queue
+
+    Dim arr() As Integer
+    Dim start As Integer = 0
+    Dim finish As Integer = 0
+    Dim size As Integer
+
+    Public Sub New(ByVal size As Integer)
+        Dim arr(size - 1) As Integer
+        Me.arr = arr
+        Me.size = size
+    End Sub
+
+    Public Sub enqueue(ByVal input As Integer)
+        arr(start) = input
+        finish += 1
+    End Sub
+
+    Public Function isFull() As Boolean
+        If finish - start + 1 = 0 Then Return True
+        Return False
+    End Function
+
+    Public Function isEmpty() As Boolean
+        If start = finish Then Return True
+        Return False
+    End Function
+
+End Class
