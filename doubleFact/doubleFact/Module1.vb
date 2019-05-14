1b78633ed48dfd3cbbdf1b9d6c4217d15b827208
18PY0606 <18PY0606@L2463>
2019-05-14 16:37:45 +0100

    double factorial

---
+++ doubleFact/doubleFact/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine(fac2(Console.ReadLine()))
+        Console.ReadKey()
+    End Sub
+
+    Function fac2(ByVal n As Integer)
+        If n <= 1 Then Return 1
+        Return fac2(n - 2) * n
+    End Function
+
+End Module
